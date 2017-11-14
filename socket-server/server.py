218c4add37b2a245e26c71b6822ae8a38ab851bb
NikhilPeri <nikhil.peri@shopify.com>
2017-11-13 20:52:15 -0500

    shit

--- socket-server/server.py
+++ socket-server/server.py
@@ -15,7 +15,7 @@ import sys
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 8080
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
