6396aa857677184231137d81fd237d65c8fb1856
NikhilPeri <nikhil.peri@shopify.com>
2017-11-13 20:50:04 -0500

    clean dir

--- socket-server/server.py
+++ socket-server/server.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 
 import socket
+import serial
+import sys
 
+#ser = serial.Serial()
+#ser.baudrate = 19200
+#ser.port = 'COM1'
+#ser.open()
+#if !ser.open():
+#    sys.exit()
+
+#print('connected to arduino on: ', ser.port)
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_PORT = 8080
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
+print ('listenting for connections on... ', TCP_IP, ':', TCP_PORT)
 
 conn, addr = s.accept()
-print ('Connection address:', addr)
-while 1:
+print ('connected to client on: ', addr)
+while True:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print ("received data:", data)
-    conn.send(data)  # echo
+    print ('forwarded: ', data)
+    #ser.write(data)
+
 conn.close()
