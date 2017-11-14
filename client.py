71f1391db145033dc9a3f8e0e9e47b7a50931ddc
Zuraiz Zafar <zuraiz.zafar97@gmail.com>
2017-11-13 21:18:40 -0500

    actual changes

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 import socket
 
 
-TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.0.100'
 TCP_PORT = 8080
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
@@ -12,6 +12,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
 while 1:
+    MESSAGE = input("")
     s.send(bytearray(MESSAGE, "utf-8"))
     data = s.recv(BUFFER_SIZE)
 s.close()
