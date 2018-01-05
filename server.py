0838252ed096bffe724e0fd4fb03e3426e6f1703
tmetade <tmetade@yahoo.ca>
2018-01-04 21:11:57 -0500

    pi server

--- server.py
+++ server.py
@@ -4,15 +4,15 @@ import socket
 import serial
 import sys
 
-ser = serial.Serial('/dev/cu.usbmodem1411')
-ser.baudrate = 19200
+ser = serial.Serial('/dev/ttyACM0')
+ser.baudrate = 9600
 if not ser.isOpen():
     sys.exit()
 
-#print('connected to arduino on: ', ser.port)
+print('connected to arduino on: ', ser.port)
 
-# TCP_IP = '192.168.0.100'
-TCP_IP = "localhost"
+TCP_IP = '192.168.0.100'
+# TCP_IP = "localhost"
 TCP_PORT = 5005
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
@@ -25,9 +25,13 @@ conn, addr = s.accept()
 print('connected to client on: ', addr)
 while True:
     data = conn.recv(BUFFER_SIZE)
-    if(data==b'119'): # w
-        print("moving forward")
-    if(data == b'127'): # ctrl + backspace
+#    if(data==b'119'): # w
+#       print("moving forward")
+    if(data == b'23'): # ctrl + backspace
         conn.close()
-
-    ser.write(data)
+    if(int(data) < 257):
+##        print(int(data))
+        ser.write(chr(int(data)))
+    else:
+        print('You Fucked UP:', int(data))
+#    print(ser.readline())
